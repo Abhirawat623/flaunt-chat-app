355311109e03a0ac6817ea9738699b02742ec17f
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-03-23 01:00:34 +0530

    signup hook + toaster

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,16 +7,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "react/prop-types":"off"
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	settings: { react: { version: "18.2" } },
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+		"react/prop-types": "off",
   },
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
 import {GenderCheckBox}from "../components/GenderCheckBox";
+import  { Toaster } from 'react-hot-toast';
+import useSignup from "../customHooks/useSignup";
 import {Link} from "react-router-dom";
 export const Signup = () => {
+  const { loading, signup } = useSignup();
     //inputs
     const [inputs,setInputs]=useState(
     {
     fullname:"",
     username:"",
     password:"",
-    confirmPassword:"",
+    confirmedPassword:"",
     gender:""
     })
     console.log(inputs)
 //gender checkbox
 const handleCheckBoxChange=(gender)=>{
   setInputs({...inputs,gender})
-
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  await signup(inputs);
+};
+
   return (
     <div className="flex flex-col items-center justify-center ">
+      <Toaster/>
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
         <h1 className="text-3xl font-semibold text-center text-gray-300 cursor-pointer">
           Sign Up <span className="text-green-500 cursor-pointer">FlauntChat</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -68,8 +77,8 @@ const handleCheckBoxChange=(gender)=>{
               type="password"
               placeholder="Confirm Password"
               className="w-full input input-bordered h-10"
-              value={inputs.confirmPassword}
-              onChange={(event)=> setInputs({...inputs,confirmPassword:event.target.value})}
+              value={inputs.confirmedPassword}
+              onChange={(event)=> setInputs({...inputs,confirmedPassword:event.target.value})}
             />
           </div>
           <GenderCheckBox onCheckBoxChange={handleCheckBoxChange} selectedGender={inputs.gender}/>
@@ -78,9 +87,9 @@ const handleCheckBoxChange=(gender)=>{
           Already our user?
           </div></Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              <span className="loading loading-dots"></span>
-            </button>
+          <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
           </div>
         </form>
       </div>
