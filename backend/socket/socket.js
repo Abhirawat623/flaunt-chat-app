005bf4c4e39d7ae444ca19b0b072ef59dab578c5
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-03-29 20:32:36 +0530

    socket context link added

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://flaunt-chat-app.vercel.app/"],
+		origin: ["http://localhost:5000/"],
 		methods: ["GET", "POST"],
 	},
 });
